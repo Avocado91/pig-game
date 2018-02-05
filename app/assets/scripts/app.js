01e814f5241a5c307a4d9fcd50e88278e6f54b2c
T.J <t.ruggles@live.ca>
2018-02-05 03:35:57 -0500

    Completed basic functionality of the game, minor fixes still required

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -8,3 +8,78 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores = [0, 0];
+let roundScore = 0;
+let activePlayer = 0;
+let dice = Math.floor(Math.random() * 6) + 1; //generate random number between 1 and 6 for the dice
+
+
+// setting up the game
+document.querySelector('.dice').style.display = 'none'; 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+	//generate random number between 1 and 6 for the dice
+	let dice = Math.floor(Math.random() * 6) + 1;
+	
+	//display the result
+	let diceDOM = document.querySelector('.dice')
+	diceDOM.style.display  = 'block';
+	diceDOM.src = 'assets/images/dice-' + dice + '.png';
+
+	//update round score IF the number wasn't 1
+	if (dice !== 1) {
+		//add score and display in current score section
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		//force end turn
+		nextPlayer();
+	}
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+	//add current score to global score
+	scores[activePlayer] += roundScore;
+	//update the UI
+	document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+	//check if player won the game 
+	if (scores[activePlayer] >= 20) {
+		//end the game
+		document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+		document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+	} else {
+		//force end turn
+		nextPlayer();	
+	}
+	
+});
+
+
+
+
+
+
+function nextPlayer() {
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; //switch between players when a player rolls a 1
+		roundScore = 0;
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+
+		document.querySelector('.dice').style.display = 'none';
+}
+
+
+
+
+
