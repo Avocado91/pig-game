3d46a6392fb1f9964e5581f94105d53c7a4a04ee
T.J <t.ruggles@live.ca>
2018-02-05 17:37:42 -0500

    Started html and css styles for challenge-rule2

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -8,8 +8,10 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 CHALLENGE RULES-----
-1. A player looses his ENTIRE score when he rolls two 6's in a row. After  that, it's the next player's turn. (Hint: Always save the previous diceroll in a separate variable).
-2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: You can read that value with the .value property in JS.).
+1. A player looses his ENTIRE score when he rolls two 6's in a row. After  that, it's the next player's turn. (Hint: Always save the previous diceroll in a separate variable). --COMPLETED
+
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: You can read that value with the .value property in JS).
+
 3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: You will need CSS to position the second dice, so take a look at the CSS code for the first one.)
 
 */
